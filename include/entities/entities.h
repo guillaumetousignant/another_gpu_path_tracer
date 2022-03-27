f559175157a534e04df8edd904d26711380c0100
Guillaume Tousignant <moi_guillaume@hotmail.com>
2022-03-27 10:02:34 -0400

    added transformation matrix

--- include/entities/entities.h
+++ include/entities/entities.h
@@ -5,12 +5,13 @@ namespace APTracer {
      * @brief Contains the fundamental types of the project. 
      * 
      * Most of the entities represent interface classes, to be specialized in their respective namespaces.
-     * Some entities, such as Vec3f, represent basic types used throughout the program.
+     * Some entities, such as Vec3, represent basic types used throughout the program.
      */
     namespace Entities {}
 }
 
 #include "Ray_t.h"
-#include "Vec3f.h"
+#include "TransformMatrix_t.h"
+#include "Vec3.h"
 
 #endif
